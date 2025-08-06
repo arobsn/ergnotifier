b7b34d21a57ff341eb33ffa1e7cb541b04fbe163
arobsn <87387688+arobsn@users.noreply.github.com>
2025-08-06 20:24:27 -0300

    feat: add .env file support

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
+use dotenvy::dotenv;
+use ergnotifier::telemetry;
+use tracing::{debug, info, warn};
+
 fn main() {
+    // Initialize the tracing subscriber
+    telemetry::init(telemetry::default_subscriber());
+
+    // Load environment variables from .env file
+    match dotenv() {
+        Ok(p) => info!(path = ?p, ".env file loaded successfully"),
+        Err(e) if e.not_found() => debug!("No .env file found, skipping."),
+        Err(e) => warn!("Could not load .env file: {:?}", e),
+    }
+
     println!("Hello, world!");
 }
