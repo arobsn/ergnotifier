cca354e6c51b519ecfb18d3e92ad9ff4c07584a2
arobsn <87387688+arobsn@users.noreply.github.com>
2025-08-05 21:25:03 -0300

    feat: add telemetry

---
+++ src/telemetry.rs
@@ -0,0 +1,21 @@
+use tracing::Subscriber;
+use tracing::subscriber::set_global_default;
+use tracing_log::LogTracer;
+use tracing_subscriber::fmt::MakeWriter;
+use tracing_subscriber::layer::SubscriberExt;
+use tracing_subscriber::{EnvFilter, Registry};
+
+pub fn get_subscriber<Sink>(env_filter: String) -> impl Subscriber + Send + Sync
+where
+    Sink: for<'a> MakeWriter<'a> + Send + Sync + 'static,
+{
+    let env_filter =
+        EnvFilter::try_from_default_env().unwrap_or_else(|_| EnvFilter::new(env_filter));
+
+    Registry::default().with(env_filter)
+}
+
+pub fn init_subscriber(subscriber: impl Subscriber + Send + Sync) {
+    LogTracer::init().expect("Failed to set logger");
+    set_global_default(subscriber).expect("Failed to set subscriber");
+}
