73346dd17c35ec6271df2a72dde81073c2b65554
arobsn <87387688+arobsn@users.noreply.github.com>
2025-08-06 20:24:33 -0300

    fix: telemetry

--- src/telemetry.rs
+++ src/telemetry.rs
@@ -1,21 +1,20 @@
+use std::env;
+
 use tracing::Subscriber;
 use tracing::subscriber::set_global_default;
 use tracing_log::LogTracer;
-use tracing_subscriber::fmt::MakeWriter;
 use tracing_subscriber::layer::SubscriberExt;
 use tracing_subscriber::{EnvFilter, Registry};
 
-pub fn get_subscriber<Sink>(env_filter: String) -> impl Subscriber + Send + Sync
-where
-    Sink: for<'a> MakeWriter<'a> + Send + Sync + 'static,
-{
+pub fn default_subscriber() -> impl Subscriber + Send + Sync {
+    let log_level = env::var("RUST_LOG").unwrap_or_else(|_| "info".to_string());
     let env_filter =
-        EnvFilter::try_from_default_env().unwrap_or_else(|_| EnvFilter::new(env_filter));
-
-    Registry::default().with(env_filter)
+        EnvFilter::try_from_default_env().unwrap_or_else(|_| EnvFilter::new(log_level));
+    let stdout_log = tracing_subscriber::fmt::layer();
+    Registry::default().with(env_filter).with(stdout_log)
 }
 
-pub fn init_subscriber(subscriber: impl Subscriber + Send + Sync) {
+pub fn init(subscriber: impl Subscriber + Send + Sync) {
     LogTracer::init().expect("Failed to set logger");
     set_global_default(subscriber).expect("Failed to set subscriber");
 }
