f11acee5c880a434eb555f6ce42314eb9ccdde20
arobsn <87387688+arobsn@users.noreply.github.com>
2025-08-08 22:06:34 -0300

    refactor: simplify telemetry setup

--- src/telemetry.rs
+++ src/telemetry.rs
@@ -4,14 +4,13 @@ use tracing::Subscriber;
 use tracing::subscriber::set_global_default;
 use tracing_log::LogTracer;
 use tracing_subscriber::layer::SubscriberExt;
-use tracing_subscriber::{EnvFilter, Registry};
+use tracing_subscriber::{EnvFilter, Registry, fmt};
 
 pub fn default_subscriber() -> impl Subscriber + Send + Sync {
-    let log_level = env::var("RUST_LOG").unwrap_or_else(|_| "info".to_string());
-    let env_filter =
-        EnvFilter::try_from_default_env().unwrap_or_else(|_| EnvFilter::new(log_level));
-    let stdout_log = tracing_subscriber::fmt::layer();
-    Registry::default().with(env_filter).with(stdout_log)
+    let log_level = env::var("RUST_LOG").unwrap_or("info".to_string());
+    Registry::default()
+        .with(EnvFilter::try_from_default_env().unwrap_or_else(|_| EnvFilter::new(log_level)))
+        .with(fmt::layer())
 }
 
 pub fn init(subscriber: impl Subscriber + Send + Sync) {
